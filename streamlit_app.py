c020fb004500b35fe6dd65391ed434f8f83dde97
PieRatCat <AmsSumner@gmail.com>
2025-03-18 11:35:34 +0100

    test page

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,9 @@ port = int(os.environ.get("PORT", 8501))
 # Define the pages
 page_1 = st.Page("page_1.py", title="About project", icon=":material/home:")
 page_2 = st.Page("page_2.py", title="App demo", icon=":material/draw:")
-test = st.Page("test.py", title="Test", icon=":material/bug_report:")
 
 # Set up navigation
-pg = st.navigation([page_1, page_2, test])
+pg = st.navigation([page_1, page_2])
 
 # Run the selected page
 pg.run()
